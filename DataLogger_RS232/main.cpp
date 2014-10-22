bc0e67734b9d360a5a51294a0420d2754fd81db4
LauraBogatu <laurabogatu@yahoo.com>
2014-10-22 12:00:34 +0100

    Write data from RS232 transceiver to terminal

---
+++ DataLogger_RS232/main.cpp
@@ -0,0 +1,36 @@
+#include "mbed.h"
+
+DigitalOut myled(LED1);
+ 
+Serial pc (USBTX, USBRX); // tx, rx
+Serial dataLogger (p9, p10);  // tx, rx
+
+char c;
+char MSserial;
+int count;
+
+int main() {    
+    
+    dataLogger.baud(38400);
+    pc.baud(57600);
+    
+    pc.printf("PC and Datalogger serial ready !!\n\r");
+    while(1) {
+        if(pc.readable()) {
+            printf("pc is readable!");
+            count=0;
+            c = pc.getc();
+            pc.printf("\n\r%c\n\r",c);
+        
+            dataLogger.printf("A\0\0");
+   
+        }
+        while (dataLogger.readable()) {
+            //printf ("datalogger readable ");
+            MSserial = dataLogger.getc();
+            pc.printf("%d, %X\n\r",count,(MSserial));
+            count++;
+        };
+     
+    }
+}
